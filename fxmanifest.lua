227c5e0f23428615dfc8a80b97dd9403ca345918
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-10-10 01:04:02 -0500

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+games { 'gta5' }
+author 'zaps6000'
+client_scripts {
+'client/*'
+}
+
+server_scripts {
+'server/*'
+}
+lua54 'yes'
+shared_scripts {
+    '@ox_lib/init.lua',
+}
